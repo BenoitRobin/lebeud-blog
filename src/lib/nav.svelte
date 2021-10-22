80a1a8f76158c8631873b340b68989cd4d2c185b
BenoitRobin <benoitrobin@me.com>
2021-10-22 16:01:38 +0200

    feat: :sparkles: add pages and nav

---
+++ src/lib/nav.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let pages
+</script>
+
+<div
+  class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box"
+>
+  <div class="px-2 mx-2 navbar-start">
+    <span class="text-lg font-bold"> lEbEud </span>
+  </div>
+  <div class="px-2 mx-2 navbar-center">
+    <div class="flex items-stretch">
+      <a
+          href="/"
+          class="btn btn-ghost btn-sm rounded-btn"
+        >
+          Home
+        </a>
+      {#each pages as {title, slug}}
+        <a
+          href={`/${slug}`}
+          class="btn btn-ghost btn-sm rounded-btn"
+        >
+          {title}
+        </a>
+      {/each}
+    </div>
+  </div>
+  <div class="navbar-end">
+  
+  </div>
+</div>
